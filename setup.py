9f27d3e108c403613359e0d0e9f2570b1540f0be
vivianikelly <viani22@gmail.com>
2024-11-30 21:58:28 -0300

    Ajustes

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="math_operations",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Viviani Pedroso",
+    author_email="viani22@gmail.com",
+    description="The package math_operations",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/vivianikelly/math-operations-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
